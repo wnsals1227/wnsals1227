fb6c1ae93e3371d4a147b4be52fa763efc235ef6
wnsals1227 <wnsals1227@daum.net>
2018-08-23 20:37:30 +0900

    de

--- test2.java
+++
@@ -1,8 +0,0 @@
-package Project;
-
-public class test2 
-{
-	public static void main(String[] args) {
-		System.out.println(1234);
-	}
-}
--- test3.java
+++
@@ -1,8 +0,0 @@
-package Project;
-
-public class test3 
-{
-	public static void main(String[] args) {
-		System.out.println(1234);
-	}
-}
